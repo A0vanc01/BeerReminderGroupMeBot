d6b93b7581b1eff00b1b9e31517994e8504a715f
AlexVance <AlexVance@hchb.com>
2018-03-26 10:30:10 -0400

    Hopefully that fixes it.

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
-    return d + datetime.timedelta(days_ahead)
+    return d + timedelta(days_ahead)
 
 def send_message(msg):
     url  = 'https://api.groupme.com/v3/bots/post'
