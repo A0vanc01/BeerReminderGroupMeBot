d3d68247ee7a52f0e62bcef7aedb81d2d44c56f1
AlexVance <AlexVance@hchb.com>
2018-03-26 10:59:23 -0400

    See if I fixed the date / datetime

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import sys
 import json
+import datetime
 import time
-from datetime import date, datetime
+from datetime import date
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -20,15 +21,14 @@ def webhook():
 
     log('Received {}'.format(data))
 
-    d = datetime.now()
+    d = datetime.datetime.now()
     #d = date.today.isoformat()
 
     next_Sunday = next_weekday(d, 6) # 0 = Monday, 1=Tuesday, 2=Wednesday...
 
     msg = '{}, you volunteered to get beer on the {}.  I will try to remind you.'.format(data['name'], next_Sunday.date())
     send_message(msg)
-    #add fireBase, or just save and read from the Herokuserver here for a remind at 12:30 on next_Suday
-    #then figure out how to read in once every .5 minutes and check if it matches the date and time, but only to YY:MM:DD:HH:MM
+    #add PYODBC and use rows = cursor.execute("select * from tmp").fetchall() # probably
 
 
     return "ok", 200
@@ -37,7 +37,7 @@ def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
-    return d + timedelta(days_ahead)
+    return d + datetime.timedelta(days_ahead)
 
 def send_message(msg):
     url  = 'https://api.groupme.com/v3/bots/post'
