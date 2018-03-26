f46c6094e3184f616cd116124d4c76cd134e8214
AlexVance <AlexVance@hchb.com>
2018-03-26 14:27:25 -0400

    checking the db

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import sys
 import json
 import datetime
 import time
-from datetime import date
+import os
+import psycopg2
 
+
+from datetime import date
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
 from flask import Flask, request
 
 application = Flask(__name__)
@@ -20,15 +22,37 @@ def webhook():
        return "ok", 200
 
     log('Received {}'.format(data))
+    
+    #Database check
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS VOLUNTEER
+       (ID INT PRIMARY KEY    NOT NULL,
+       NAME            TEXT    NOT NULL,
+       DATE_ENTERED    DATE    NOT NULL,
+       DATE_REQUIRED   DATE    NOT NULL;''')
 
     d = datetime.datetime.now()
     #d = date.today.isoformat()
 
     next_Sunday = next_weekday(d, 6) # 0 = Monday, 1=Tuesday, 2=Wednesday...
 
+    conn.execute('''INSERT INTO VOLUNTEER (NAME, DATE_ENTERED, DATE_REQUIRED)
+                 VALUES ({}, {}, {}'''.format(data['name'],d , next_Sunday.date()))
+    
+
     msg = '{}, you volunteered to get beer on the {}.  I will try to remind you.'.format(data['name'], next_Sunday.date())
     send_message(msg)
-    #add PYODBC and use rows = cursor.execute("select * from tmp").fetchall() # probably
+    
+    #getting all the stuff in the DB
+    rows = cur.fetchall()
+    testMessage = "\nShow me the databases:\n"
+    for row in rows:
+        testMessage += "   ", row[0]
+    msg = 'here is what the db has in it: {},'.format(testMessage)
+    send_message
 
 
     return "ok", 200
