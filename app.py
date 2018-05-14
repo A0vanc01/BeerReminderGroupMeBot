37d9b78abe9bd5955fa80ad2b4515ac4bdb2a672
AlexVance <AlexVance@hchb.com>
2018-05-14 12:31:11 -0400

    checking Zack's poll feature, and taking out SQLAlchemy stuff to restart

--- app.py
+++ app.py
@@ -4,58 +4,89 @@ import json
 import datetime
 import time
 import os
-import psycopg2
-
+import random
 
 from datetime import date
+from groupme.polls import Poll, PollOption, PollHelper
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from flask import Flask, request
 
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
 application = Flask(__name__)
+
+GROUPME_API_URL = 'https://api.groupme.com/v3/bots/post'
+GROUPME_BOT_ID = os.getenv('GROUPME_BOT_ID')
+GROUPME_ACCESS_TOKEN = os.getenv('GROUPME_ACCESS_TOKEN')
+
+poll_helper = PollHelper(GROUPME_ACCESS_TOKEN)
+
 
 @application.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     log(data)
-    if data['sender_type'] == 'bot' or data['text'] != "/beer":
-       return "ok", 200
+
+    if (handle_message(data)):
+        return "ok poll response", 200
+
+    if data['sender_type'] == 'bot' or "beer" not in data['text']:# != "/beer":
+       return "ok not beer response", 200
 
     log('Received {}'.format(data))
     
     #Database check
     DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS VOLUNTEER
-    (ID SERIAL PRIMARY KEY  NOT NULL,
-    NAME            TEXT    NOT NULL,
-    DATE_ENTERED    TIMESTAMP DEFAULT NULL,
-    DATE_REQUIRED   DATE    NOT NULL)""")
     d = datetime.datetime.now()
 
     next_Sunday = next_weekday(d, 6) # 0 = Monday, 1=Tuesday, 2=Wednesday...
-    cur.execute("""
-    INSERT INTO VOLUNTEER (NAME, DATE_ENTERED, DATE_REQUIRED)
-    VALUES (%(str)s, %(date)s, %(date)s);
-    """,
-    {'str': data['name'], 'datetime':d , 'date': next_Sunday.date()})
+    # cur.execute("""
+    # INSERT INTO VOLUNTEER (NAME, DATE_ENTERED, DATE_REQUIRED)
+    # VALUES (%(str)s, %(date)s, %(date)s);
+    # """,
+    # {'str': data['name'], 'datetime':d , 'date': next_Sunday.date()})
 
     msg = '{}, you volunteered to get beer on the {}.  I will try to remind you.'.format(data['name'], next_Sunday.date())
     send_message(msg)
     
     #getting all the stuff in the DB
-    cur.execute("SELECT * FROM VOLUNTEER")
-    rows = cur.fetchall()
+
+    # cur.execute("SELECT * FROM VOLUNTEER")
+    # rows = cur.fetchall()
     testMessage = "\nShow me the databases:\n"
-    for row in rows:
-        testMessage += "   ", row[0]
+    # for row in rows:
+    #testMessage += "   ", row[0]
     msg = 'here is what the db has in it: {},'.format(testMessage)
     send_message
 
-    conn.commit()
+    #conn.commit()
     return "ok", 200
+
+def handle_message(message) -> bool:
+
+    if message['sender_type'] != 'user':
+        # Don't process bot messages for now
+        return False
+    
+    for attachment in message['attachments']:
+        if attachment['type'] == 'poll':
+            group_id = message['group_id']
+            #app.logger.debug('I found a poll')
+            poll: Poll = poll_helper.get_poll(group_id, attachment['poll_id'])
+            send_message(f"That poll's title is {poll.subject}")
+            my_vote: PollOption = random.choice(poll.options)
+            resp = poll_helper.vote(group_id, poll, my_vote)
+            send_message(f'I pick "{my_vote.title}"')
+            #app.logger.debug(resp)
+            return True
+
+    send_message('Hello, ' + message['name'])
+
+    return False
 
 def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
@@ -63,19 +94,18 @@ def next_weekday(d, weekday):
         days_ahead += 7
     return d + datetime.timedelta(days_ahead)
 
-def send_message(msg):
-    url  = 'https://api.groupme.com/v3/bots/post'
-
-    data = {
-        'bot_id' : os.getenv('GROUPME_BOT_ID'),
-        'text'   : msg,
-        }
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
-  
 def log(msg):
     print(str(msg))
     sys.stdout.flush()
+
+def send_message(message):
+    data = {
+        'bot_id' : GROUPME_BOT_ID,
+        'text' : message,
+    }
+    #app.logger.debug('Sending: ' + str(data))
+    request = Request(GROUPME_API_URL, urlencode(data).encode())
+    urlopen(request).read().decode()
 
 if __name__=="__main__":
     application.run()
